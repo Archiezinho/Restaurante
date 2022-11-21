5961c64b121759685a431dfb48f8c7ec7cb53d30
Matheus Lopes <bellzinho1221@gmail.com>
2022-11-21 19:35:16 -0300

    epa

--- classes/conexao.php
+++ classes/conexao.php
@@ -7,3 +7,19 @@ define('DB','db_restaurante');
 
 $conexao = mysqli_connect(HOST,USUARIO,SENHA,DB) or die ('Não foi possível se conectar');
 ?>
+
+
+<?php
+
+$host = 'mysql:host=localhost;dbname=db_restaurante';
+$user = 'root';
+$senha = '';
+
+try{
+    $conexao = new PDO($host,$user,$senha);
+}
+catch(PDOException $e){
+    print_r($e);
+}
+
+?>
