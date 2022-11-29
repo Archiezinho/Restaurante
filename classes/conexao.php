99040e3781310cc4428716083e25d9632e3a9249
Matheus Lopes <bellzinho1221@gmail.com>
2022-11-29 15:06:45 -0300

    Update conexao.php

--- classes/conexao.php
+++ classes/conexao.php
@@ -8,17 +8,5 @@ define('DB','db_restaurante');
 $conexao = mysqli_connect(HOST,USUARIO,SENHA,DB) or die ('Não foi possível se conectar');
 
 
-
-abstract class conexao{
-    
-    protected function conectaDB(){
-        try{
-            $conexao = new PDO('mysql:host=localhost;dbname=restaurante','root','');
-            return $conexao;
-        }
-        catch(PDOException $e) {
-            return $e->getMessage();
-        }
-    }
-}
+$conexao = new PDO('mysql:host=localhost;dbname=restaurante','root','');
 ?>
